7d35e53891e656b03d71ff2051f9c0a1d211c149
dymafr <erwan1309@hotmail.com>
2022-06-26 14:49:58 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-20">
       <input ref={inputRef} className="m-10" type="text" />
-      <button className="btn btn-primary" onChange={handleClick}>
+      <button className="btn btn-primary" onClick={handleClick}>
         Test
       </button>
     </div>
